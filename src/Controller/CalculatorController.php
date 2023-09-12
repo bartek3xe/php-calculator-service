398f24b91708531bc6ed04d287e33e917c387940
bartek3xe <bartek3xe@gmail.com>
2023-09-12 22:03:28 +0200

    Split code, create handleForm method

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Form\CalculatorType;
 use App\Service\CalculatorService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,17 +22,24 @@ class CalculatorController extends AbstractController
         $form = $this->createForm(CalculatorType::class);
         $form->handleRequest($request);
 
+        $formResult = $this->handleForm($form);
+
+        return $this->render('calculator/index.html.twig', [
+            'form'   => $form,
+            'result' => $formResult ?? ''
+        ]);
+    }
+
+    private function handleForm(FormInterface $form): ?int
+    {
         if ($form->isSubmitted() && $form->isValid()) {
             $operator     = $form->getData()['operator'];
             $firstNumber  = $form->getData()['firstNumber'];
             $secondNumber = $form->getData()['secondNumber'];
 
-            $result = $this->calculator->calculate($operator, $firstNumber, $secondNumber);
+            return $this->calculator->calculate($operator, $firstNumber, $secondNumber);
         }
 
-        return $this->render('calculator/index.html.twig', [
-            'form'   => $form,
-            'result' => $result ?? ''
-        ]);
+        return null;
     }
 }
